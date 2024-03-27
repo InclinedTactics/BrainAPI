6177ed4b10df449fd2137c94e269f7169c9e5736
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-03-27 12:52:28 -0500

    Initial Commit

---
+++ BrainAPI/BrainAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainAPIApp.swift
+//  BrainAPI
+//
+//  Created by J. DeWeese on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BrainAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
